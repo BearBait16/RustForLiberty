34c6875afefb4440cf6b66965cf758e907e04948
Jackson Aden <jacksonaden@gmail.com>
2025-03-02 19:14:39 -0700

    Added the reqwest library and other needed libraries to the cargo.toml, and changed the code to just get the request, and it totally worked!

--- rust-for-liberty/src/main.rs
+++ rust-for-liberty/src/main.rs
@@ -1,21 +1,7 @@
-use error_chain::error_chain;
-use std::io::Read;
+use reqwest;
 
-error_chain! {
-    foreign_links{
-        Io(std::io::Error);
-        HttpRequest(reqwest::Error);
-    }
-}
-
-fn main() -> Result<()> {
-    let mut res = reqwest::blocking::get("https://helldiverstrainingmanual.com/api/v1/war/status")?;
-    let mut body = String::new();
-    res.read_to_string(&mut body)?;
-
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
-    println!("Body:\n{}", body);
-
-    Ok(())
+#[tokio::main]
+async fn main() {
+    let result = reqwest::get("https://helldiverstrainingmanual.com/api/v1/war/status").await;
+    println!("{:?}", result);
 }
