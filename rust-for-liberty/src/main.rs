1e6a7c2a1d1658f9870ab575274392e4f290ab15
Jackson Aden <jacksonaden@gmail.com>
2025-03-03 14:03:46 -0700

    Added some more http request lines of code

--- rust-for-liberty/src/main.rs
+++ rust-for-liberty/src/main.rs
@@ -1,7 +1,12 @@
+// Tutorial from https://blog.logrocket.com/making-http-requests-rust-reqwest/
+
 use reqwest;
 
 #[tokio::main]
 async fn main() {
-    let result = reqwest::get("https://helldiverstrainingmanual.com/api/v1/war/status").await;
-    println!("{:?}", result);
+    let news = reqwest::get("https://helldiverstrainingmanual.com/api/v1/war/news").await.unwrap().text().await;
+    println!("{:?}", news);
+
+    let active_planets = reqwest::get("https://helldiverstrainingmanual.com/api/v1/war/campaign").await.unwrap().text().await;
+    println!("{:?}", activePlanets);
 }
